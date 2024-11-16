5c0d3199c3722258aaa2f0cb35a6e2b4ff5118a5
blwond <blwond@outlook.es>
2024-11-16 22:02:28 +0100

    [+] Updated PostCard

--- composeApp/src/commonMain/kotlin/com/quehacerenzaragoza/soydezaragoza/presentation/components/news/PostCard.kt
+++ composeApp/src/commonMain/kotlin/com/quehacerenzaragoza/soydezaragoza/presentation/components/news/PostCard.kt
@@ -1,15 +1,20 @@
 package com.quehacerenzaragoza.soydezaragoza.presentation.components.news
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,71 +29,101 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import com.quehacerenzaragoza.soydezaragoza.data.model.post.Post
+import com.quehacerenzaragoza.soydezaragoza.util.extensions.getRelativeTimeFromNow
 import com.quehacerenzaragoza.soydezaragoza.util.extensions.removeHtmlTags
+import compose.icons.FeatherIcons
+import compose.icons.feathericons.Clock
+import compose.icons.feathericons.Heart
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun PostCard(post: Post) {
-    Card(
+    Box(
         modifier = Modifier
             .fillMaxWidth()
             .padding(vertical = 8.dp)
-            .height(200.dp),
-        shape = RoundedCornerShape(8.dp)
+            .height(120.dp)
     ) {
-        Box(modifier = Modifier.fillMaxSize()) {
-            post._embedded.wp_FeaturedMedia?.firstOrNull()?.let { featuredMedia ->
-                AsyncImage(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .clip(RoundedCornerShape(8.dp)),
-                    model = featuredMedia.source_url,
-                    contentDescription = "user image",
-                    contentScale = ContentScale.Crop
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.spacedBy(40.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .align(Alignment.CenterVertically)
+            ) {
+                Text(
+                    text = post.primary_category,
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = Color.Gray,
+                    modifier = Modifier.padding(bottom = 8.dp)
                 )
+
+                Text(
+                    text = post.title.rendered,
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.primary,
+                    fontWeight = FontWeight.Bold,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        imageVector = FeatherIcons.Clock,
+                        contentDescription = "Clock",
+                        tint = Color.Gray,
+                        modifier = Modifier.height(14.dp)
+                    )
+
+                    Spacer(modifier = Modifier.width(4.dp))
+
+                    Text(
+                        text = getRelativeTimeFromNow(post.date),
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = Color.Gray
+                    )
+
+                    Spacer(modifier = Modifier.width(8.dp))
+
+                    Icon(
+                        imageVector = FeatherIcons.Heart,
+                        contentDescription = "Heart",
+                        tint = Color.Gray,
+                        modifier = Modifier.height(14.dp)
+                    )
+
+                    Spacer(modifier = Modifier.width(4.dp))
+
+                    Text(
+                        text = "23",
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = Color.Gray
+                    )
+                }
             }
 
-            Text(
-                text = "Fecha: ${post.date}",
-                color = Color.White,
+            Card(
                 modifier = Modifier
-                    .align(Alignment.TopStart)
-                    .background(Color(0xAA000000), shape = RoundedCornerShape(4.dp))
-                    .padding(horizontal = 8.dp, vertical = 4.dp)
-            )
-
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .align(Alignment.BottomStart)
-                    .background(
-                        Brush.verticalGradient(
-                            colors = listOf(Color.Transparent, Color.Black),
-                            startY = 50f
-                        )
-                    )
+                    .size(95.dp)
+                    .clip(RoundedCornerShape(10.dp))
+                    .align(Alignment.CenterVertically)
             ) {
-                Column(
-                    modifier = Modifier
-                        .align(Alignment.BottomStart)
-                        .padding(16.dp)
-                ) {
-                    Text(
-                        text = post.title.rendered,
-                        style = MaterialTheme.typography.titleLarge,
-                        color = MaterialTheme.colorScheme.background,
-                        maxLines = 2,
-                        overflow = TextOverflow.Ellipsis
-                    )
-
-                    Text(
-                        text = post.excerpt.rendered.removeHtmlTags(),
-                        color = MaterialTheme.colorScheme.background,
-                        maxLines = 2,
-                        overflow = TextOverflow.Ellipsis
+                post._embedded.wp_FeaturedMedia?.firstOrNull()?.let { featuredMedia ->
+                    AsyncImage(
+                        model = featuredMedia.source_url,
+                        contentDescription = "Post Image",
+                        modifier = Modifier.fillMaxSize(),
+                        contentScale = ContentScale.Crop
                     )
                 }
             }
         }
     }
 }
+
+
